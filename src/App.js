95df5526d29b2c977629c61d326c6fadb00b7247
patrozada <patriciarozada@gmail.com>
2019-05-24 20:55:36 +0200

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,58 +1,77 @@
-import React from 'react';
-import MainPage from './components/MainPage'
-import './App.css';
+import React from "react";
+import MainPage from "./components/MainPage";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      allCharacters :[],
-      filterValue :'',
-      filteredCharacters:[],
-    }
-    this.getData = this.getData.bind(this);
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.getData()
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			allCharacters: [],
+			filterValue: "",
+			filteredCharacters: []
+		};
+		this.getData = this.getData.bind(this);
+		this.handleInputChange = this.handleInputChange.bind(this);
+		this.getData();
+	}
 
-  getData(){
-    fetch('http://hp-api.herokuapp.com/api/characters')
-      .then(response => response.json())
-      .then(characters =>
-        this.setState({
-          allCharacters: characters
-        }))
-  }
+	getData() {
+		fetch("http://hp-api.herokuapp.com/api/characters")
+			.then(response => response.json())
+			.then(characters =>
+				this.setState({
+					allCharacters: characters
+				})
+			);
+	}
 
-  filterData(filter){
-    const { allCharacters } = this.state;
-    const filteredCharacters = allCharacters.filter(character => character.name.includes(filter))
-    return filteredCharacters
-  }
+	filterData(filter) {
+		const { allCharacters } = this.state;
+		const filteredCharacters = allCharacters.filter(character =>
+			character.name.includes(filter)
+		);
+		return filteredCharacters;
+	}
 
-  handleInputChange(event){
-    const newFilterValue = event.currentTarget.value;  
-    this.setState(prevState =>{
-      return{
-        ...prevState,
-        filterValue:newFilterValue,
-        filteredCharacters:this.filterData(newFilterValue)
-      }
-    })
-  }
+	handleInputChange(event) {
+		const newFilterValue = event.currentTarget.value;
+		this.setState(prevState => {
+			return {
+				...prevState,
+				filterValue: newFilterValue,
+				filteredCharacters: this.filterData(newFilterValue)
+			};
+		});
+	}
 
-  render() {
-    return (
-      <div className="App">
-        <MainPage
-          characterDefaultList = {this.state.allCharacters}
-          characterFilteredList = {this.state.filteredCharacters}
-          value = {this.state.filterValue}
-          methodInputchange = {this.handleInputChange}
-        />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className='App'>
+				<Switch>
+          <Route 
+            path='/' 
+            render = {routerProps => (
+              <MainPage 
+                match = {routerProps.match}
+                characterDefaultList={this.state.allCharacters}
+                characterFilteredList={this.state.filteredCharacters}
+                value={this.state.filterValue}
+                methodInputchange={this.handleInputChange}
+              />
+            )}
+           />
+				</Switch>
+			</div>
+		);
+	}
 }
 
 export default App;
+
+{/* <MainPage
+  characterDefaultList={this.state.allCharacters}
+  characterFilteredList={this.state.filteredCharacters}
+  value={this.state.filterValue}
+  methodInputchange={this.handleInputChange}
+/> */}
