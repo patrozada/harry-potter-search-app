0ec6401cbd7f671fd462d03725fb45793c9671c0
patrozada <patriciarozada@gmail.com>
2019-05-25 18:12:44 +0200

    Indent code

--- src/components/DetailCard.js
+++ src/components/DetailCard.js
@@ -1,23 +1,36 @@
-import React from 'react';
-import './Card.scss';
+import React from "react";
+import "./Card.scss";
+import { Link } from "react-router-dom";
 
-class DetailCard extends React.Component{
-  render (){
-    const {characterDefaultList, match}=this.props
-    const {id} = match.params
-    const selectedChar = characterDefaultList.find(character=>character.uuid === parseInt(id))
-    return(selectedChar)?(
-      <div>
-        Ayyyyy
-        <h4>{selectedChar.name}</h4>
-        <div className="imageContainer" style={{backgroundImage:`url(${selectedChar.image})`}}/>  
-        <h5>{selectedChar.house}</h5>
-        <h5>patronus:{selectedChar.patronus}</h5>
-        <h5>{selectedChar.alive}</h5>
-      </div>
-    ) : (
-      <p>Our appologies! Character not found</p>
-    )
-  }
+class DetailCard extends React.Component {
+	render() {
+		const { characterDefaultList, match } = this.props;
+		const { id } = match.params;
+		const selectedChar = characterDefaultList.find(
+			character => character.uuid === parseInt(id)
+		);
+		return selectedChar ? (
+			<div>
+				<h4>{selectedChar.name}</h4>
+				<div
+					className='imageContainer'
+					style={{ backgroundImage: `url(${selectedChar.image})` }}
+				/>
+				<h5>{selectedChar.house}</h5>
+				<h5>patronus:{selectedChar.patronus}</h5>
+				<h5>{selectedChar.alive}</h5>
+				<Link to='/'>
+					<button type='button'>Back to Home</button>
+				</Link>
+			</div>
+		) : (
+			<div>
+				<p>Our appologies! Character not found</p>
+				<Link to='/'>
+					<button type='button'>Back to Home</button>
+				</Link>
+			</div>
+		);
+	}
 }
 export default DetailCard;
