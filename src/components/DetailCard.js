f83179c07f3c127611a7d5b0aa992bb77c3e07a5
patrozada <patriciarozada@gmail.com>
2019-05-25 17:44:17 +0200

    Prevent error on reload

--- src/components/DetailCard.js
+++ src/components/DetailCard.js
@@ -6,16 +6,17 @@ class DetailCard extends React.Component{
     const {characterDefaultList, match}=this.props
     const {id} = match.params
     const selectedChar = characterDefaultList.find(character=>character.uuid === parseInt(id))
-     console.log(selectedChar.alive)
-    return(
+    return(selectedChar)?(
       <div>
         Ayyyyy
         <h4>{selectedChar.name}</h4>
         <div className="imageContainer" style={{backgroundImage:`url(${selectedChar.image})`}}/>  
         <h5>{selectedChar.house}</h5>
-        <h5>{selectedChar.patronus}</h5>
+        <h5>patronus:{selectedChar.patronus}</h5>
         <h5>{selectedChar.alive}</h5>
       </div>
+    ) : (
+      <p>Our appologies! Character not found</p>
     )
   }
 }
